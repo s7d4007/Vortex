79c841f7dea5d216ae1e710cfff7a286501b2432
s7d4007 <souvikdutta0775@outlook.com>
2026-09-05 22:09:40 +0530

    Implement search_term function in InvertedIndex class

--- src/inverted_index.cpp
+++ src/inverted_index.cpp
@@ -5,5 +5,9 @@ void InvertedIndex::add_term(const std::string& term, int doc_id, double term_fr
 };
 
 std::vector<Posting> InvertedIndex::search_term(const std::string& term) {
-    
+    auto it = index.find(term);
+    if(it != index.end()){
+        return it->second;
+    }
+    return{};
 }
